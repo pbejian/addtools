7cd5fac5bf17dd1255738397f7b7321883a57abb
Pierre BEJIAN <pbejian@free.fr>
2023-01-16 08:56:20 +0100

    tralala

---
+++ algebra.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def somme(n):
+    """
+    Calcule la somme des entiers de 1 à n.
+    """
+    T = np.linspace(1, n, num=n, dtype="int")
+    return T.sum()
+
+print(somme(3))
